1605a01abe6db8c8746f0a090bae376fe2007648
Vimal0204 <80668288+Vimal0204@users.noreply.github.com>
2023-06-09 23:14:04 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,6 @@
+package com.stunnedjava;
+
+public interface Area {
+      public void circleArea();
+
+}
--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
 package com.stunnedjava;
 
-import addition.sum;
+public class Main  {
+    public static class CalculteArea implements Area{
 
-public class Main {
-
+        @Override
+        public void circleArea() {
+           double result = 3.14*12*12;
+            System.out.println("Area of the circle is "+result);
+        }
+    }
     public static void main(String[] args) {
-	    sum s = new sum();
-        s.Sum(5,8);
+	    CalculteArea area = new CalculteArea();
+        area.circleArea();
     }
 }
