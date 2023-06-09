b8da606fc1a35858ce387cdff73a3ab3bacb8b84
Vimal0204 <80668288+Vimal0204@users.noreply.github.com>
2023-06-09 23:12:03 +0530

    Add files via upload

---
+++ mul.java
@@ -0,0 +1,13 @@
+package mul;
+
+public class mul {
+    int x;
+    int y;
+    public mul(int x,int y) {
+        this.x=x;
+        this.y=y;
+    }
+    public int cross(){
+        return x*y;
+    }
+}
